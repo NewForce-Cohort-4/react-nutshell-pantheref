636a667f8f1d31d465c17884de54cc2a1f2adcca
jon-xo <jonn@hey.com>
2021-03-10 14:07:27 -0500

    Added files and functions to enable authorization.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
-import Nutshell from './components/Nutshell'
+import { Nutshell } from "./components/Nutshell";
 import './index.css'
 
+// Import Nutshell function and print to DOM.
 ReactDOM.render(
   <Router>
       <Nutshell />
