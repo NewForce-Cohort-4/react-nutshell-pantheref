17d071422ec3db82eb86e3be8d6150b2151f16a9
Thomas Frazier <tfrazier2424@gmail.com>
2021-03-12 16:28:50 -0500

    message window prints with time stamp, user, and auto scroll feature

--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -1,5 +1,20 @@
 import React from "react"
 
-export const MessageCard = ({message}) => (
-    <p className="message">{message.message}</p>
-)
+export const MessageCard = ({message}) => {
+    console.log(message)
+
+      const formatTime = (timestamp) => {
+        const adjustedDate = new Intl.DateTimeFormat("en", {
+          dateStyle: "short",
+          timeStyle: "medium",
+        }).format(timestamp);
+        // console.log(adjustedDate);
+        return adjustedDate;
+      };
+    return (
+    <p className="message">User:{message.user.name} Message:{message.message}
+    {formatTime(message.timeStamp)}
+    </p>
+
+    )
+}
--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -1,11 +1,12 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect, useState, useRef } from "react"
 import { MessageContext } from "./MessageProvider"
 import { MessageCard } from "./Message"
 import { useHistory, useParams } from "react-router-dom"
+import "./message.css"
 
 export const MessageList = () => {
     //This state changes when " getMessages()" is invoked below
-    const { messages, getMessages, addMessages, getUsers } = useContext(MessageContext)
+    const { messages, getMessages, addMessages } = useContext(MessageContext)
 
     //wait for data before button is active
     const [isLoading, setIsLoading] = useState(true)
@@ -13,18 +14,25 @@ export const MessageList = () => {
     const { messageId } = useParams()
     // const history = useHistory()
 
+    const fieldRef = useRef(null)  
+
+
     // initial state of the messages holding an empty message
     const [message, setMessage] = useState({
         message:"",
         userId: "",
-        id: ""
+        id: "",
+        timeStamp: Date.now()
     })
+
+    
+
     //useEffect is going to reach out and grab getMessages and pull the stored messages back
     useEffect(() => {
     console.log("MessageList: useEffect - getMessages")    
     getMessages()
-
-},  [])
+    fieldRef.current.scrollIntoView()
+    },  [])
 
 const handleControlledInputChange = (event) => {
     //creating a copy of the state object and then set it to state
@@ -40,24 +48,32 @@ const handleClickSaveMessage = () => {
         window.alert("Please enter a message")
     } else {
         addMessages ({
-            message: message.message 
+            message: message.message,
+            userId: localStorage.getItem("nutshell_user")
+        }).then(() => {
+          fieldRef.current.scrollIntoView()
         })
         }
     }
 
-
+// let messageBody = ""
 
 return (
-    <>
+  <>
     <h2>Messages</h2>
-    <div className="message_window">
+    <div className="container-fluid">
+      <div className="message_window">
         {console.log("MessageList: Render", messages)}
         {
-        messages.map(messages => {
-                return <MessageCard key={messages.id} message={messages} />
-            })
+          messages.map((messages) => {
+            return <MessageCard key={messages.id} message={messages} />;
+          })
+          // {messageBody.scrollTop=1000}
         }
-    <fieldset>
+        <div className="test" ref={fieldRef}></div>
+      </div>
+
+      <fieldset>
         <div className="message_form">
           <input
             type="text"
@@ -67,21 +83,21 @@ return (
             autoFocus
             className="form-control"
             placeholder="type a message"
-             value={message.message}
+            value={message.message}
           />
         </div>
       </fieldset>
-      <button
-        className="btn btn-primary"
-        // disabled={isLoading}
-        onClick={(event) => {
-          event.preventDefault(); // Prevent browser from submitting the form and refreshing the page
-          handleClickSaveMessage();
-        }}
-      >
-        Send Message
-      </button>
     </div>
-    </>
-)
+    <button
+      className="btn btn-primary"
+      // disabled={isLoading}
+      onClick={(event) => {
+        event.preventDefault(); // Prevent browser from submitting the form and refreshing the page
+        handleClickSaveMessage();
+      }}
+    >
+      Send Message
+    </button>
+  </>
+);
 }
