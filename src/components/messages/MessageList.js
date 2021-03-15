bda0f102efe9e1784a23b9416b2128cc1137b6ea
Thomas Frazier <tfrazier2424@gmail.com>
2021-03-15 11:29:46 -0400

    added feature to clear message input each time one is sent

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -58,6 +58,8 @@ const handleClickSaveMessage = (event) => {
             userId: localStorage.getItem("nutshell_user")
         }).then(() => {
           fieldRef.current.scrollIntoView()
+          //clearing the message after one is sent
+          setMessage({message: ""})
         })
         }
     }
